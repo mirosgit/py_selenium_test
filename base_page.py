79826ec43f6c1bb0d54609dcf8271ce4b821304f
mirosgit <SgtKurosaki@gmail.com>
2022-06-13 03:48:54 +0300

    autotest_v1

--- base_page.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
