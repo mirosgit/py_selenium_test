beea735941c9862287f6da8fa2138d9fad8d0439
mirosgit <SgtKurosaki@gmail.com>
2022-06-18 01:06:16 +0300

    autotest_v8

--- base_page.py
+++ base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
+import pytest
 import time
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,19 @@
 from .product_page import Product
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', ["promo=offer0",
+                                  "promo=offer1",
+                                  "promo=offer2",
+                                  "promo=offer3",
+                                  "promo=offer4",
+                                  "promo=offer5",
+                                  "promo=offer6",
+                                  "promo=offer7",
+                                  "promo=offer8",
+                                  "promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?{link}"
     page = Product(browser, link)
     page.open()
     page.go_to_product_page()
